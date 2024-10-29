e6e72683f3316cc2b4c2a2529bc06b740754a331
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-10-29 18:02:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atc_tracker',
+    version='0.1.0',
+    description='The Air traffic Control in your terminal',
+    author='Luckyluka17',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'atc_tracker=atc_tracker.main:main',
+        ],
+    },
+    install_requires=[
+        "colorama",
+        "FlightRadarAPI",
+        "keyboard",
+        "pytz",
+        "setuptools"
+    ],
+)
