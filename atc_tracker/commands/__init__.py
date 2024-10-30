36c14f27ef893ed42e7185006b0cd123376942ad
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-10-30 17:24:07 +0100

    Update __init__.py

--- atc_tracker/commands/__init__.py
+++ atc_tracker/commands/__init__.py
@@ -1,7 +1,7 @@
-from .flights import flights_list # isort:skip
-from .airport import airport_details # isort:skip
-from .help import help # isort:skip
-from .about import about # isort:skip
+from .flights import flights_list
+from .airport import airport_details
 from .track import track_flight
+from .help import help
+from .about import about
 
 # __all__ = []
