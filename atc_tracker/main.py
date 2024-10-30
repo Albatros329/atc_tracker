a064874e587834b8d6a99c9682c80907e397adac
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-10-30 17:23:37 +0100

    Distribution of utility functions in the utils.py file

--- atc_tracker/main.py
+++ atc_tracker/main.py
@@ -1,41 +1,11 @@
 import sys
 from colorama import init, Fore, Back
 from atc_tracker.commands import *
+from .utils import *
 
 init(autoreset=True)
 
 available_filters = ["airline", "bounds", "registration", "aircraft_type"]
-
-def gradient_text(text, start_color, end_color):
-    """
-    Generates a gradient-colored text string for terminal output.
-    Args:
-        text (str): The text to be colored.
-        start_color (tuple): The RGB color tuple (R, G, B) for the start of the gradient.
-        end_color (tuple): The RGB color tuple (R, G, B) for the end of the gradient.
-    Returns:
-        str: The text with gradient colors applied, formatted for terminal output.
-    """
-
-
-    if len(text) == 0: 
-        return ""
-    
-    result = ""
-    length = len(text)
-    
-    r_step = (end_color[0] - start_color[0]) / length
-    g_step = (end_color[1] - start_color[1]) / length
-    b_step = (end_color[2] - start_color[2]) / length
-    
-    for i, char in enumerate(text):
-        r = int(start_color[0] + (r_step * i))
-        g = int(start_color[1] + (g_step * i))
-        b = int(start_color[2] + (b_step * i))
-        
-        result += f"\033[38;2;{r};{g};{b}m{char}"
-    
-    return result + Style.RESET_ALL
 
 start_color = (0, 100, 255)
 end_color = (0, 255, 100)
